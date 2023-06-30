04fd7fc492346c485c4e26a29c8b9f6db30ab0d4
Derek Buckley <dgbuckley@protonmail.com>
2023-06-29 22:03:09 -0400

    initialize

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "A Nix-flake-based Zig development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }: let
+    inherit (nixpkgs) lib;
+    genSystems =
+      lib.genAttrs
+      [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+    pkgsFor = genSystems (system: import nixpkgs {inherit system;});
+  in {
+    devShells = genSystems (system: let
+      pkgs = pkgsFor.${system};
+    in {
+      default = pkgs.mkShell {
+        packages = with pkgs; [zig zls];
+
+        shellHook = ''
+          echo "zig `${pkgs.zig}/bin/zig version`"
+        '';
+      };
+    });
+  };
+}
